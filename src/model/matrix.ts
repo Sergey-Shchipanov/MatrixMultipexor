ca618089b40abb36e0ee5af2fbc4a59abc168483
Sergei Shchipanov <abcd@bk.ru>
2020-11-27 16:26:50 +0200

    matrix model and generator

---
+++ src/model/matrix.ts
@@ -0,0 +1,59 @@
+export class Matrix {
+
+    private readonly _matrix: number[][];
+    private readonly _rowsCount: number;
+    private readonly _columnsCount: number;
+
+    constructor(rowCount: number, columnCount: number) {
+        this._rowsCount = rowCount;
+        this._columnsCount = columnCount;
+        this._matrix = new Array<number[]>();
+    }
+
+
+    get rowsCount(): number {
+        return this._rowsCount;
+    }
+
+    get columnsCount(): number {
+        return this._columnsCount;
+    }
+
+    getElement(row: number, column: number): number {
+        if (row >= this._rowsCount || column >= this._columnsCount) {
+            throw new Error("Index out of bounds");
+        }
+        return this._matrix[row][column];
+    }
+
+    addRow(elements: number[]) {
+        if (elements.length !== this._columnsCount) {
+            throw new Error("Invalid number of elements");
+        }
+        this._matrix.push(elements);
+    }
+
+    getRow(row: number): number[] {
+        if (row >= this._rowsCount) {
+            throw new Error("Index out of bounds");
+        }
+        const resultRow: number[] = [];
+
+        for (let i: number = 0; i < this._columnsCount; i++) {
+            resultRow.push(this.getElement(row, i));
+        }
+        return resultRow;
+    }
+
+    getColumn(column: number): number[] {
+        if (column >= this._rowsCount) {
+            throw new Error("Index out of bounds");
+        }
+        const resultColumn: number[] = [];
+
+        for (let i: number = 0; i < this._rowsCount; i++) {
+            resultColumn.push(this.getElement(i, column));
+        }
+        return resultColumn;
+    }
+}
--- src/services/matrix.generator.ts
+++ src/services/matrix.generator.ts
@@ -1,3 +1,17 @@
+import {Matrix} from "../model/matrix";
+
 export class MatrixGenerator {
 
+    generateMatrix(rowCount: number, columnCount: number): Matrix {
+        const matrix: Matrix = new Matrix(rowCount, columnCount);
+
+        for (let i: number = 0; i < rowCount; i++) {
+            const row: number[] = [];
+            for (let j: number = 0; j < columnCount; j++) {
+                row.push(Number((Math.random() * 10).toFixed(1)));
+            }
+            matrix.addRow(row);
+        }
+        return matrix;
+    }
 }
